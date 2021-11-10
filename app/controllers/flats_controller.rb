3e5eec60f4d3ec33bc00a474fbe09ff49fea7f7d
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-11-11 00:24:09 +0400

    Added index, new and create backend

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -9,6 +9,13 @@ class FlatsController < ApplicationController
     end
 
     def create
-    
+        @flats = Flat.new(params_flat)
+        @flats.save
+    end
+
+    private
+
+    def params_flat
+        params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
     end
 end
